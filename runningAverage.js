c42dd08d51926036700cbbe5c8c76c1a5daa2dc2
Anton Kosar <anton_kosar@epam.com>
2022-06-01 15:50:20 +0200

    refactor: replace setter func with .push

--- runningAverage.js
+++ runningAverage.js
@@ -1,10 +1,9 @@
 const runningAverage = () => {
-    let numbersArray = [];
-    const setNumbersArray = (newNumber) => numbersArray = [...numbersArray, newNumber];
+    const numbersArray = [];
 
     return (number) => {
-        if (typeof number === 'number') {
-            setNumbersArray(number);
+        if (typeof number === "number") {
+            numbersArray.push(number);
         }
         const sumOfNumbers = numbersArray.reduce((acc, n) => acc + n, 0);
         const averageNumber = sumOfNumbers / numbersArray.length;
@@ -14,9 +13,9 @@ const runningAverage = () => {
 };
 
 const rAvg = runningAverage();
-console.log(rAvg(10)) // 10.0;
-console.log(rAvg(11)) // 10.5;
-console.log(rAvg(12)) // 11.0;
-console.log(rAvg(0)) // 8.3;
-console.log(rAvg('banana')); // 8.3
+console.log(rAvg(10)); // 10.0;
+console.log(rAvg(11)); // 10.5;
+console.log(rAvg(12)); // 11.0;
+console.log(rAvg(0)); // 8.3;
+console.log(rAvg("banana")); // 8.3
 console.log(rAvg()); // 8.3
