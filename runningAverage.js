510ae4c63c282e6fa725aa6e54fb50beb3bf6c1b
Anton Kosar <anton_kosar@epam.com>
2022-06-01 14:18:29 +0200

    fix: change guardian approach

--- runningAverage.js
+++ runningAverage.js
@@ -3,14 +3,13 @@ const runningAverage = () => {
     const setNumbersArray = (newNumber) => numbersArray = [...numbersArray, newNumber];
 
     return (number) => {
-        if (number) {
+        if (typeof number === 'number') {
             setNumbersArray(number);
-            const sumOfNumbers = numbersArray.reduce((acc, n) => acc + n, 0);
-            const averageNumber = sumOfNumbers / numbersArray.length;
-
-            return averageNumber.toFixed(1);
         }
-        return  null;
+        const sumOfNumbers = numbersArray.reduce((acc, n) => acc + n, 0);
+        const averageNumber = sumOfNumbers / numbersArray.length;
+
+        return averageNumber.toFixed(1);
     };
 };
 
@@ -18,3 +17,6 @@ const rAvg = runningAverage();
 console.log(rAvg(10)) // 10.0;
 console.log(rAvg(11)) // 10.5;
 console.log(rAvg(12)) // 11.0;
+console.log(rAvg(0)) // 8.3;
+console.log(rAvg('banana')); // 8.3
+console.log(rAvg()); // 8.3
