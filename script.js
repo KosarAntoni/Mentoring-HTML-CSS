76732d8153da0c972aad8b13895650f2849bd535
Anton Kosar <anton_kosar@epam.com>
2022-06-06 16:15:39 +0200

    refactor: create performanceCheck function

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
 const rowsCount = 100;
 const colsCount = 100;
 
-// each cell
-(() => {
+const performanceCheck = (elementId, callback) => {
   const timeBegin = new Date();
-  const table = document.getElementById("table-1");
 
-  const handleCellClick = (e) => {
-    e.target.classList.add("selected");
-  };
-
-  for (let i = 0; i < rowsCount; i++) {
-    const tableRow = document.createElement("tr");
-
-    for (let i = 0; i < colsCount; i++) {
-      const tableCell = document.createElement("td");
-      tableCell.innerText = i;
-      tableCell.addEventListener("mousedown", handleCellClick);
-
-      tableRow.appendChild(tableCell);
-    }
-
-    table.appendChild(tableRow);
-  }
+  callback();
 
   const timeEnd = new Date();
   const time = document.createElement("h2");
   time.innerText = timeEnd - timeBegin;
-  table.appendChild(time);
+  elementId.appendChild(time);
+};
+
+// each cell
+(() => {
+  const table = document.getElementById("table-1");
+
+  const generateTable = () => {
+    const handleCellClick = (e) => {
+      e.target.classList.add("selected");
+    };
+
+    for (let i = 0; i < rowsCount; i++) {
+      const tableRow = document.createElement("tr");
+
+      for (let i = 0; i < colsCount; i++) {
+        const tableCell = document.createElement("td");
+        tableCell.innerText = i;
+        tableCell.addEventListener("mousedown", handleCellClick);
+
+        tableRow.appendChild(tableCell);
+      }
+
+      table.appendChild(tableRow);
+    }
+  };
+
+  performanceCheck(table, generateTable);
 })();
 
 // whole table
 (() => {
-  const timeBegin = new Date();
+  const table = document.getElementById("table-2");
 
-  const handleCellClick = (e) => {
-    if (e.target.nodeName === "TD") {
-      e.target.classList.add("selected");
+  const generateTable = () => {
+    const handleCellClick = (e) => {
+      if (e.target.nodeName === "TD") {
+        e.target.classList.add("selected");
+      }
+    };
+
+    table.addEventListener("mousedown", handleCellClick, false);
+
+    for (let i = 0; i < rowsCount; i++) {
+      const tableRow = document.createElement("tr");
+
+      for (let i = 0; i < colsCount; i++) {
+        const tableCell = document.createElement("td");
+        tableCell.innerText = i;
+
+        tableRow.appendChild(tableCell);
+      }
+
+      table.appendChild(tableRow);
     }
   };
 
-  const table = document.getElementById("table-2");
-  table.addEventListener("mousedown", handleCellClick, false);
-
-  for (let i = 0; i < rowsCount; i++) {
-    const tableRow = document.createElement("tr");
-
-    for (let i = 0; i < colsCount; i++) {
-      const tableCell = document.createElement("td");
-      tableCell.innerText = i;
-
-      tableRow.appendChild(tableCell);
-    }
-
-    table.appendChild(tableRow);
-  }
-
-  const timeEnd = new Date();
-  const time = document.createElement("h2");
-  time.innerText = timeEnd - timeBegin;
-  table.appendChild(time);
+  performanceCheck(table, generateTable);
 })();
