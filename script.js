538b5a808cce9c2a0c6648818eab460db1a94299
Anton Kosar <anton_kosar@epam.com>
2022-06-06 16:05:36 +0200

    feat: add table generator with eventlistener on whole table

--- script.js
+++ script.js
@@ -29,3 +29,35 @@ const colsCount = 100;
   time.innerText = timeEnd - timeBegin;
   table.appendChild(time);
 })();
+
+// whole table
+(() => {
+  const timeBegin = new Date();
+
+  const handleCellClick = (e) => {
+    if (e.target.nodeName === "TD") {
+      e.target.classList.add("selected");
+    }
+  };
+
+  const table = document.getElementById("table-2");
+  table.addEventListener("mousedown", handleCellClick, false);
+
+  for (let i = 0; i < rowsCount; i++) {
+    const tableRow = document.createElement("tr");
+
+    for (let i = 0; i < colsCount; i++) {
+      const tableCell = document.createElement("td");
+      tableCell.innerText = i;
+
+      tableRow.appendChild(tableCell);
+    }
+
+    table.appendChild(tableRow);
+  }
+
+  const timeEnd = new Date();
+  const time = document.createElement("h2");
+  time.innerText = timeEnd - timeBegin;
+  table.appendChild(time);
+})();
