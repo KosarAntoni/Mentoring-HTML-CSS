bb0a06c042d520921ef99d28a1c02a48120a9bc8
Anton Kosar <anton_kosar@epam.com>
2022-06-14 17:34:13 +0200

    feat: add Triangle.ts script

--- src/Point.ts
+++ src/Point.ts
@@ -7,18 +7,18 @@ export class Point {
         this.y = y;
     }
 
-    toString() {
+    toString(): string {
         return `(${this.x}, ${this.y})`
     }
 
-    distance(...args: (number | Point)[]) {
+    distance(...args: (number | Point)[]): number {
         if (!args.length) {
             return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
         }
 
         if (args.length === 1) {
             const [point] = args;
-            if (!(point instanceof Point)) return null
+            if (!(point instanceof Point)) return 0;
 
             return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
         }
@@ -26,10 +26,11 @@ export class Point {
         if (args.length === 2) {
             const [x, y] = args;
             const isNumber = (value: number | object): boolean => (!(typeof value === "object") && !isNaN(value));
-            if (!isNumber(x) && !isNumber(y)) return null;
+            if (!isNumber(x) && !isNumber(y)) return 0;
 
             return Math.sqrt(Math.pow((this.x - +x), 2) + Math.pow((this.y - +y), 2));
         }
-        return null
+
+        return 0
     }
 }
--- src/Shape.ts
+++ src/Shape.ts
@@ -1,3 +1,30 @@
+import { Point } from "./Point";
+
 export abstract class Shape {
+    protected color: string;
+    protected filled: boolean;
+    public points: Point[];
+
+    constructor(points: Point[], color: string = 'green', filled: boolean = true) {
+        this.points = points;
+        this.color = color;
+        this.filled = filled;
+
+        if (points.length < 3) throw new Error('Shape should contain at least 3 Points');
+    }
+
+    public toString(): string {
+        const points = this.points.map(point => point.toString()).join(", ");
+        const filled = this.filled ? "" : "not "
+        return `A Shape with color of ${this.color} and ${filled}filled. Points: ${points}.`;
+    }
+
+    public getPerimeter(): number {
+        return this.points.reduce((accumulator, point, index) => {
+            const distance: number = this.points[index + 1] ? point.distance(this.points[index + 1]) : point.distance(this.points[0])
+            return accumulator + distance;
+        }, 0)
+    }
+
     abstract getType(): string;
 }
