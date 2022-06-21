05b51077fef2b2927738c0df7886a2e59aeaccae
Anton Kosar <anton_kosar@epam.com>
2022-06-21 14:28:23 +0200

    feat: add Shipper class

--- package-handling.ts
+++ package-handling.ts
@@ -3,6 +3,11 @@ type packageType = {
     marks: string[];
 } & Record<'toAddress' | 'fromAddress' | 'toZipCode' | 'fromZipCode', string>;
 
+type shipperType = {
+    cost: number;
+    zipCode: number[];
+} & Record<'name' | 'base', string>;
+
 const mock: packageType = {
     weight: 20,
     fromZipCode: '92021',
@@ -11,6 +16,28 @@ const mock: packageType = {
     toAddress: '1313 Mockingbird Lane, Tulsa, OK',
     marks: ['MARK FRAGILE', 'MARK DO NOT LEAVE IF ADDRESS NOT AT HOME', 'MARK RETURN RECEIPT REQUESTED'],
 };
+
+const shippers: shipperType[] = [
+    {
+        zipCode: [1, 2, 3],
+        name: 'Air East',
+        base: 'Atlanta',
+        cost: 39
+    },
+    {
+        zipCode: [4, 5, 6],
+        name: 'Chicago Sprint',
+        base: 'a suburb of Chicago',
+        cost: 42
+
+    },
+    {
+        zipCode: [7, 8, 9],
+        name: 'Pacific Parcel',
+        base: 'San Diego',
+        cost: 51
+    },
+]
 
 class ShipmentId {
     static id: number = 0;
@@ -23,6 +50,24 @@ class ShipmentId {
 
     reset(): void {
         ShipmentId.id = 0;
+    }
+}
+
+class Shipper {
+    private _shippers: shipperType[];
+
+
+    constructor() {
+        this._shippers = shippers;
+    }
+
+    getShipper(packageZipCode: string): shipperType {
+        return this._shippers.filter(({ zipCode }) => zipCode.includes(+packageZipCode.charAt(0)))[0];
+    }
+
+    getCost(packageWeight: number, packageZipCode: string): number {
+        const shipper = this.getShipper(packageZipCode);
+        return shipper.cost * packageWeight;
     }
 }
 
@@ -39,7 +84,9 @@ class Shipment {
     }
 
     getCost(): number {
-        return this._package.weight * 0.39;
+        const { weight, toZipCode } = this._package;
+        const shipper = new Shipper();
+        return shipper.getCost(weight, toZipCode)
     }
 
     ship() {
